adf5a9509afbee70d6c8d05eb55889d2fbf26f00
KR2304 <144369153+KR2304@users.noreply.github.com>
2023-09-22 18:23:09 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
+def fact_rec(n):
+  if(n==0)or(n==1):
+    return 1
+  else:
+    return n*fact_rec(n-1)
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting.")
+number=int(input("Enter a value:"))
+res=fact_rec(number)
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
-
-# Create objects for Batsman and Bowler
-batsman = Batsman()
-bowler = Bowler()
-
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+print("The factorial of {} is {}".format(number,res))
